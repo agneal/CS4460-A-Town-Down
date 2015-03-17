50f770bf7b04f46b13ce7243b3ff97dce7d2083d
Patrick Conner <pconner03@gmail.com>
2015-03-17 13:25:08 -0400

    Converted data to proper format

---
+++ dataFormatConverter.py
@@ -0,0 +1,26 @@
+import json
+oldData = open("data.json")
+
+jOldData = json.load(oldData)
+ 
+newData = {}
+
+for yearStateOLD in jOldData:
+	curYear = yearStateOLD["Year"]
+	if not curYear in newData:
+		newData[curYear] = {}
+	yearStateNEW = {}
+
+	for key in yearStateOLD:
+		if key != "Year" and key != "State":
+			yearStateNEW[key] = yearStateOLD[key]
+	newData[curYear][yearStateOLD["State"]] = yearStateNEW
+
+
+
+jNewData = open("data_new.json", "w")
+
+json.dump(newData, jNewData)
+
+jNewData.close()
+oldData.close()
