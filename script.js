e223827de5d9c0092688fa8c15a62b2181ba349a
Patrick Conner <pconner03@gmail.com>
2015-04-04 19:28:01 -0400

    Two visualizations on same page. Still need to work out map coloring

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ VIOLENT_CRIMES = ["Murder and Non-Negligent Manslaughter", "Forcible Rape", "Rob
 PROPERTY_CRIMES = ["Burglary", "Larceny-Theft", "Motor Vehicle"];
 //State regions from http://en.wikipedia.org/wiki/List_of_regions_of_the_United_States#Census_Bureau-designated_regions_and_divisions
 STATE_REGION = {"Alabama": "South", "Alaska": "West", "Arizona": "West", "Arkansas": "South", "California": "West", "Colorado": "West", "Connecticut": "Northeast", "Delaware": "South", "District of Columbia": "South", "Florida": "South", "Georgia": "South", "Hawaii": "West", "Idaho": "West", "Illinois": "Midwest", "Indiana": "Midwest", "Iowa": "Midwest", "Kansas": "Midwest", "Kentucky": "South", "Louisiana": "South", "Maine": "Northeast", "Maryland": "South", "Massachusetts": "Northeast", "Michigan": "Midwest", "Minnesota": "Midwest", "Mississippi": "South", "Missouri": "Midwest", "Montana": "West", "Nebraska": "Midwest", "Nevada": "West", "New Hampshire": "Northeast", "New Jersey": "Northeast", "New Mexico": "West", "New York": "Northeast", "North Carolina": "South", "North Dakota": "Midwest", "Ohio": "Midwest", "Oklahoma": "South", "Oregon": "West", "Pennsylvania": "Northeast", "Rhode Island": "Northeast", "South Carolina": "South", "South Dakota": "Midwest", "Tennessee": "South", "Texas": "South", "Utah": "West", "Vermont": "Northeast", "Virginia": "South", "Washington": "West", "West Virginia": "South", "Wisconsin": "Midwest", "Wyoming": "West"};
+STATE_SYMBOLS = {"Alabama": "AL", "Alaska": "AK", "American Samoa": "AS", "Arizona": "AZ", "Arkansas": "AR", "California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE", "District Of Columbia": "DC", "Federated States Of Micronesia": "FM", "Florida": "FL", "Georgia": "GA", "Guam": "GU", "Hawaii": "HI", "Idaho": "ID", "Illinois": "IL", "Indiana": "IN", "Iowa": "IA", "Kansas": "KS", "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Marshall Islands": "MH", "Maryland": "MD", "Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS", "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV", "New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY", "North Carolina": "NC", "North Dakota": "ND", "Northern Mariana Islands": "MP", "Ohio": "OH", "Oklahoma": "OK", "Oregon": "OR", "Palau": "PW", "Pennsylvania": "PA", "Puerto Rico": "PR", "Rhode Island": "RI", "South Carolina": "SC", "South Dakota": "SD", "Tennessee": "TN", "Texas": "TX", "Utah": "UT", "Vermont": "VT", "Virgin Islands": "VI", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV", "Wisconsin": "WI", "Wyoming": "WY"};
+STATE_COLORS = {"0":"#fee5d9", "1":"#fcbba1", "2":"#fc9272", "3":"#fb6a4a", "4":"#ef3b2c", "5":"#cb181d", "6":"#99000d", "defaultFill":"#aaa"};
+NUM_DIVISIONS = 7;
 var UNEMPLOYMENT = "Unemployment";
+
+
 
 
 var currentYearData = null;
@@ -31,6 +36,19 @@ var chart = d3.select(".chart")
 	.attr("height", height + margin.top + margin.bottom)
 	.append("g") 
 	.attr("transform", "translate(" + (margin.left) + "," + margin.top + ")");
+
+var map = new Datamap({
+	element: document.getElementById("map"),
+	scope: 'usa',
+	fills: STATE_COLORS,
+	data: {},
+	geographyConfig: {
+		borderColor: "#444",
+		highlightOnHover: true,
+		highlightFillColor: "#aaebff",
+		highlightBorderColor: '#000'
+	}
+});
 
 var dodScatter = d3.select("body").append("div")
     .attr("class", "tooltip")
@@ -76,6 +94,10 @@ var radius = d3.scale.linear();
 var cValue = function(d){return STATE_REGION[d["Name"]];};
 var colors = d3.scale.category10();
 
+var yCategoryMin = null;
+var yCategoryMax = null;
+
+
 function plotInit(){
 	//Domains
 	xScale.domain([0.0, 19.0]); //ONLY go to 25% for more interesting vis
@@ -89,12 +111,13 @@ function plotInit(){
 
 	var minPop = null;
 	var maxPop = null;
-	for(var year in DATA){//Maybe do this year by year, and adjust axes accordingly
+	for(var year in DATA){
 		delete DATA[year]["United States"]; //Ignore
 		for(var state in DATA[year]){
 			var rViolent = violentCrimeRate(DATA[year][state]);
 			var pop = DATA[year][state]["Population"];
 			//TODO - property
+			//Actually, no recalculate y axis domain when its variable is changed
 			if(minViolent === null || rViolent < minViolent)
 				minViolent = rViolent;
 			if(maxViolent === null || rViolent > maxViolent)
@@ -105,7 +128,9 @@ function plotInit(){
 			if(maxPop === null || pop > maxPop)
 				maxPop = pop;
 		}
-	}
+	}//TODO - overhaul this thing
+	yCategoryMax = maxViolent;
+	yCategoryMin = minViolent;
 	yScale.domain([minViolent - 50, maxViolent + 50]);
 	radius.domain([0, maxPop]).range([7,22]);
 
@@ -160,8 +185,8 @@ function dataUnpacker(d){
 function drawMarks(data){
 	// debugger;
 	currentYearData = data[currentYear];
-	console.log("currentYear");
-	console.log(currentYearData);
+	// console.log("currentYear");
+	// console.log(currentYearData);
 	// chart.transition().duration(2);
 	// chart.selectAll(".dot").remove(); //TODO - transition?
 	chart.selectAll(".dot")
@@ -207,6 +232,7 @@ function drawMarks(data){
 		.attr("dy", ".35em")
 		.style("text-anchor", "end")
 		.text(function(d) {return d;})
+	updateMarks();
 	
 }
 
@@ -227,8 +253,34 @@ function updateMarks(){
 		.attr("cx", xMap)
 		.attr("cy", yMap);
 		// .style("fill", "blue");
+	var currentYearData = DATA[currentYear];
 
-	console.log("transition");
+	// console.log("transition");
+	var mapColors = {};
+	// console.log(currentYearData);
+
+
+	var max = null;
+	var min = null;
+	for(var state in currentYearData){
+		var val = yValue(currentYearData[state]);
+		if(max == null || max < val)
+			max = val;
+		if(min == null || min > val)
+			min = val;
+	}//Is this necessary? Or can we use global (regardless of year) min/max for the yValue
+
+	for(var state in currentYearData){
+		var value = yValue(currentYearData[state]);
+
+		var idx = ((value - min) / (yCategoryMax - min)) * (NUM_DIVISIONS-1);
+		idx = (Math.ceil(idx));//I'm not really feelin' this way of doing divisions
+		//TODO - replace ^ 
+		console.log(idx);
+		mapColors[STATE_SYMBOLS[state]] = {"fillKey" : idx+""};
+	}
+	// console.log(colors)
+	map.updateChoropleth(mapColors);
 }
 
 function main(error, data){
